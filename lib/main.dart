059612f5c8f113bbe1c040600cdbecf6bffc0097
Md Mehedi Hasan Nayeem <nayeem15-5049@diu.edu.bd>
2025-02-18 12:52:58 +0600

    nike air image

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,91 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Basic UI -02",
-      home: LabClass05(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Product Display'),
+          backgroundColor: Colors.black,
+        ),
+        body: Center(
+          child: ProductCard(),
+        ),
+      ),
     );
   }
 }
 
-class LabClass05 extends StatelessWidget {
-  const LabClass05({super.key});
-
+class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Basic Flutter Design -02"),
+    return Container(
+      padding: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(color: Colors.black12, blurRadius: 5, spreadRadius: 2),
+        ],
       ),
-      body: Column(
+      width: 350,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Align(
-            alignment: Alignment.centerLeft, // Aligns to the left
-            child: Text(
-              " 4.0",
-              style: TextStyle(
-                fontSize: 40,
+            alignment: Alignment.topLeft,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Text(
+                '50% OFF',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
             ),
           ),
+          SizedBox(height: 10),
+          Image.network(
+            'https://nikearprod.vtexassets.com/arquivos/ids/1217812-800-800?width=800&height=800&aspect=true',
+            height: 100,
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Nike Air Shoes',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(
-                Icons.star,
-                size: 50,
+              Text(
+                '\$500',
+                style: TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                  color: Colors.grey,
+                ),
               ),
-              Icon(
-                Icons.star,
-                size: 50,
-              ),
-              Icon(
-                Icons.star,
-                size: 50,
-              ),
-              Icon(
-                Icons.star,
-                size: 50,
-              ),
-              Icon(
-                Icons.star_outline,
-                size: 50,
+              SizedBox(width: 10),
+              Text(
+                '\$450',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
               ),
             ],
           ),
+          SizedBox(height: 10),
         ],
       ),
     );
