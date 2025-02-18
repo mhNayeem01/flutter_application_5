72ba836c4c5d7156d34923bddd9757fb6c784500
mhNayeem01 <nayeem15-5049@diu.edu.bd>
2025-02-18 09:32:29 +0600

    initi

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     title: "Basic UI -02",
+     home: Scaffold(
+      appBar: AppBar(
+        title: Text("Basic Flutter Design "),
+      ),
+     ), 
+    ),
+  }
+}
